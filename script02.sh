013fdd9e64b170b2dbb7d2d8ebb35cc65c7f3a11
YamilPalomares <yamil.palomares@uan.edu.mx>
2024-05-22 02:03:04 +0000

    Add files via upload

---
+++ script02.sh
@@ -0,0 +1,7 @@
+# Autor: Yamil Palomares
+# Fecha: 07-03-24
+# Descripción: Manejo de parametros dentro de la invocación de un Script
+#!/bin/bash
+
+echo "El nombre del script es: $0" 
+echo "Los argumentos son: $1 - $2 - $3"
---
+++ script04.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Autor: Yamil Palomares
+# Fecha: 07-03-24
+# Descripción: Ejemplo de una función 
+#!/bin/bash
+
+function aqui {
+	 echo "Hola mundo $1"
+
+}
+aqui $1 
